0438c5b1c3c3cc516a2312774d01d83ff6ff76e1
devGeonil <jgi92@naver.com>
2019-04-22 23:34:18 +0900

    app

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const flash = require("flash");
+
+const app = express();
+
+app.set('view engine','pug');
+app.set('views', path.join(__dirname,'views'));
+app.set('port', process.env.PORT || 8001);
+
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser('nodebirdsecret'));
+app.use(session({
+  resave:false,
+  saveUninitalized:false,
+  secret:'nodebirdsecret',
+  cookie:{
+    httpOnly: true,
+    secure:false
+  }
+}))
+app.use(flash());
+
+
+app.listen(app.get('port'), () =>{
+  console.log(`${app.get('port')}번 포트에서 서버 실행중 입니다.`);
+});
